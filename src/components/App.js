8a46b1962304c711fa0cb1cb0dba74322e9af92a
Aleisson <caioaleisson@gmail.com>
2022-09-13 18:38:44 -0300

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login/login";
 // import Registration from "./Login/Registration";
 // import GlobalStyle from "../styles/globalStyles";
+
+
+
+console.log(process.env.REACT_APP_URL_PROJECT)
 
 export default function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import dotenv from 'dotenv';
+//REACT_APP_URL_PROJECT
+dotenv.config();
 
 ReactDOM.render(<App />, document.querySelector(".root"));
