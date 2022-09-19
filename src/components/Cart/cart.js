5105861c9553f50b095fd12bf0d0bfe22c2bfbd2
isael lopes <isael.alves@turucui.ufpa.br>
2022-09-18 22:56:43 -0300

    front cart ok

--- src/components/Cart/cart.js
+++ src/components/Cart/cart.js
@@ -1,70 +1,81 @@
-import img from "../../assets/img/81ibfYk4qmL.jpg";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { BsCartDash } from "react-icons/bs";
+import { IoArrowUndo } from "react-icons/io5";
+import React, { useEffect, useState } from "react";
+import { getBooksCart } from "../../services/bookstore.services";
+import { AuthContext } from "../common/auth";
 
 function Cart() {
+  const { dados } = React.useContext(AuthContext);
+  const [books, setBooks] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const promise = getBooksCart(dados.token);
+
+    promise.then((res) => {
+      setBooks(res.data);
+    });
+
+    promise.catch((e) => console.error(e));
+  }, []);
+
+  function AddBooks() {
+    if (books.length > 0) {
+      return books.map((book, i) => {
+        const { name, imagem, valor, author } = book;
+
+        return (
+          <li key={i}>
+            <img src={imagem} alt="" />
+            <section>
+              <h2>{name}</h2>
+              <h2>{author}</h2>
+              <h2>R$ {valor}</h2>
+            </section>
+            <div>
+              <BsCartDash />
+            </div>
+          </li>
+        );
+      });
+    } else {
+      return <h1>Carrinho vazio</h1>;
+    }
+  }
+
   return (
     <>
-      <Title>Meu carrinho de Compras</Title>
+      <Title>
+        <IoArrowUndo onClick={() => navigate("../products")} />
+        <h1>Meu carrinho de Compras</h1>
+      </Title>
       <ListBooks>
-        <li>
-          <img src={img} alt="" />
-          <section>
-            <h2>Harry Potter e a pedra filosofal</h2>
-            <h2>R$ 80,00</h2>
-          </section>
-          <div>
-            <BsCartDash />
-          </div>
-        </li>
-
-        <li>
-          <img src={img} alt="" />
-          <section>
-            <h2>Harry Potter e a pedra filosofal</h2>
-            <h2>R$ 80,00</h2>
-          </section>
-          <div>
-            <BsCartDash />
-          </div>
-        </li>
-
-        <li>
-          <img src={img} alt="" />
-          <section>
-            <h2>Harry Potter e a pedra filosofal</h2>
-            <h2>R$ 80,00</h2>
-          </section>
-          <div>
-            <BsCartDash />
-          </div>
-        </li>
-
-        <li>
-          <img src={img} alt="" />
-          <section>
-            <h2>Harry Potter e a pedra filosofal</h2>
-            <h2>R$ 80,00</h2>
-          </section>
-          <div>
-            <BsCartDash />
-          </div>
-        </li>
+        <AddBooks />
       </ListBooks>
-      <Button>Comprar</Button>
+      <Button onClick={() => navigate("../checkOut")}>Comprar</Button>
     </>
   );
 }
 
 export default Cart;
 
-const Title = styled.h1`
-  font-family: "Roboto Slab", serif;
-  text-align: center;
-  font-size: 20px;
-  font-weight: 700;
-  line-height: 25px;
-  margin: 30px auto;
+const Title = styled.header`
+  svg {
+    width: 17px;
+    height: 17px;
+    margin: 15px 0 0 15px;
+  }
+
+  h1 {
+    font-family: "Roboto Slab", serif;
+    text-align: center;
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 25px;
+    margin: 20px auto;
+  }
 `;
 
 const ListBooks = styled.ul`
@@ -73,11 +84,12 @@ const ListBooks = styled.ul`
   align-items: center;
 
   li {
+    position: relative;
     width: 320px;
     display: flex;
     margin-bottom: 15px;
     border-radius: 5px;
-    padding:10px;
+    padding: 10px;
     color: #000000;
     background-color: #ffffff;
     img {
@@ -86,15 +98,17 @@ const ListBooks = styled.ul`
       margin-right: 5px;
     }
     h2 {
-      margin-top: 15px;
       font-family: "Roboto Slab", serif;
       font-size: 15px;
       font-weight: 500;
       line-height: 17px;
     }
 
-    div{
-      width: 40px;
+    div {
+      position: absolute;
+      top: 10px;
+      right: 0;
+      width: 60px;
       height: 140px;
       display: flex;
       align-items: center;
--- src/services/bookstore.services.js
+++ src/services/bookstore.services.js
@@ -1,32 +1,39 @@
 import axios from "axios";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
+const BASE_URL = process.env.REACT_APP_URL_PROJECT + "";
 
-const BASE_URL = process.env.REACT_APP_URL_PROJECT+""
-
-function signUp(body){
-    const promise = axios.post(`${BASE_URL}/signUp`,body)
-    return promise;
+function signUp(body) {
+  const promise = axios.post(`${BASE_URL}/signUp`, body);
+  return promise;
 }
 
-function signIN(body){
-    const promise = axios.post(`${BASE_URL}/signIn`,body)
-    return promise;
+function signIN(body) {
+  const promise = axios.post(`${BASE_URL}/signIn`, body);
+  return promise;
 }
 
-
-function getProducts(token){
-    const promise = axios.get(`${BASE_URL}/products`,{ headers: {"Authorization" : `Bearer ${token}`}});
-    return promise;
-
+function getProducts(token) {
+  const promise = axios.get(`${BASE_URL}/products`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return promise;
 }
 
-function postCart(body, token){
-    const promise = axios.post(`${BASE_URL}/cart`, body,{ headers: {"Authorization" : `Bearer ${token}`}})
-    return promise;
+function postCart(body, token) {
+  const promise = axios.post(`${BASE_URL}/cart`, body, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return promise;
 }
 
+function getBooksCart(token) {
+  const promise = axios.get(`${BASE_URL}/cart`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return promise;
+}
 
-export{signUp, signIN, getProducts, postCart}
+export { signUp, signIN, getProducts, postCart, getBooksCart };
